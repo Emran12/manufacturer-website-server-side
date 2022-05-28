c6ed46f6d3ace7dd1770a2bf0819cc08146afce3
Emran12 <emran6452@gmail.com>
2022-05-28 07:24:16 +0600

    added get api for storing reviews

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ async function run() {
     const orderCollection = client
       .db("computer-accessories")
       .collection("orders");
+    const reviewCollection = client
+      .db("computer-accessories")
+      .collection("reviews");
 
     app.get("/accessories", async (req, res) => {
       const query = {};
@@ -41,11 +44,41 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/order", async (req, res) => {
       const order = req.body;
-      console.log("ordre is ", order);
       const result = await orderCollection.insertOne(order);
       console.log(`A document was inserted with id ${result.insertedId}`);
+    });
+
+    app.put("/accessories/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const data = req.body;
+      const prevQnty = data.availableQnty;
+      const bookedQnty = data.bookedQnty;
+      const updatedData = prevQnty - bookedQnty;
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          availableQnty: updatedData,
+        },
+      };
+      const result = await accessoryCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
+      console.log(
+        `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
+      );
+      res.send(result);
     });
   } finally {
   }
